184d3928eae12e17bfea811734ea9d94e0dacf6a
Сергей Шумилов <wumilovsergey@gmail.com>
2023-07-01 07:50:22 +0300

    author model

--- users/models/author.py
+++ users/models/author.py
@@ -1,36 +1,46 @@
 from django.db import models
 from django.utils import timezone
 
-
-class Author(models.Model):
-    """Модель автора"""
-
-    author_id = models.AutoField(
-        primary_key=True,
-        verbose_name="ID автора"
-    )
-    name = models.CharField(
-        verbose_name="Имя автора",
-        max_length=150,
-        unique=True,
-        null=True,
-        blank=True
-    )
-    email = models.EmailField(
-        blank=False,
-        null=False,
-        verbose_name="Почта автора",
-        help_text="Email"
-    )
-    date_joined = models.DateTimeField(
-        verbose_name="Дата регистрации",
-        default=timezone.now
-    )
-    is_active = models.BooleanField(
-        verbose_name="Активный",
-        default=True
-    )
-
-    class Meta:
-        verbose_name = "Автор"
+class Author(models.Model): 
+    """Модель автора""" 
+ 
+    author_id = models.PositiveIntegerField( 
+        primary_key=True, 
+        verbose_name="ID автора" 
+    ) 
+ 
+    author_name = models.CharField( 
+        max_length=150, 
+        verbose_name="Ник" 
+    ) 
+ 
+    name = models.CharField( 
+        max_length=150,  
+        verbose_name="Имя", 
+        unique=True 
+    ) 
+ 
+    surname = models.CharField( 
+        max_length=150,  
+        verbose_name="Фамилия" 
+    ) 
+ 
+    age = models.PositiveIntegerField( 
+        verbose_name="Возраст"
+    ) 
+ 
+    email = models.EmailField() 
+ 
+    date_joined = models.DateField( 
+        verbose_name="Дата регистрации", 
+        default=timezone.now 
+    ) 
+ 
+    is_active = models.BooleanField( 
+        verbose_name="Активный", 
+        default=True 
+    ) 
+ 
+    class Meta: 
+        verbose_name = "Автор" 
         verbose_name_plural = "Авторы"
